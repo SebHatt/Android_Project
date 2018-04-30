d225efc7231b29a8577386aaa4d308e7222556a3
SebHatt <hatt.sebastian@gmail.com>
2018-04-30 14:47:30 +0100

    Haptic_Feedback_Working_Text_Fade_In_Working

--- YouAreApp/app/src/main/java/com/example/seb/youareapp/YouAreAppActivity.java
+++ YouAreApp/app/src/main/java/com/example/seb/youareapp/YouAreAppActivity.java
@@ -1,11 +1,15 @@
 package com.example.seb.youareapp;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.seb.youareapp.R.id.scanButtonId;
 
@@ -14,22 +18,26 @@ public class YouAreAppActivity extends AppCompatActivity {
 
     private TextView stateTextView;
     private Button scanButton;
+    private Vibrator myVib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
         setContentView(R.layout.activity_you_are_app);
 
 
         stateTextView = findViewById(R.id.stateTextViewId);
         scanButton = findViewById(R.id.scanButtonId);
 
+        final Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(3000);
 
 
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View button) {
-                stateTextView.setText("Hold Scan Button Longer!");
+                stateTextView.setText("Hey! Press Longer!");
 
             }
         });
@@ -42,15 +50,21 @@ public class YouAreAppActivity extends AppCompatActivity {
         scanButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View button) {
+                myVib.vibrate(1000);
                 StateProvider stateProvider = new StateProvider();
                 YouAreApp youAreApp = new YouAreApp(stateProvider);
                 String randomState = youAreApp.randomState();
                 stateTextView.setText(randomState);
+                stateTextView.startAnimation(in);
                 return true;
             }
         });
 
     }
+
+
+
+
 }
 
 
