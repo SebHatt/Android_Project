6c4ac9fe6913b526fc71cffa7a997652eecb2104
SebHatt <hatt.sebastian@gmail.com>
2018-05-01 17:21:28 +0100

    Camera_Test_App_Added_Kinda_Works_But_Wont_Default_To_Front_Camera

---
+++ PictureDemo/app/src/main/java/com/example/seb/picturedemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.seb.picturedemo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.provider.MediaStore.EXTRA_OUTPUT;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCamera = findViewById(R.id.btnCamera);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra("android.intent.extras.CAMERA_FACING", 1);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+}
--- YouAreApp/app/src/main/java/com/example/seb/youareapp/StateProvider.java
+++ YouAreApp/app/src/main/java/com/example/seb/youareapp/StateProvider.java
@@ -13,18 +13,33 @@ public class StateProvider {
 
     public void setUpStates(){
         this.states = new ArrayList();
-        states.add("Today you should be thankful.");
-        states.add("Today you will get lucky :)");
-        states.add("Today you need to practice frugality.");
+        states.add("You should be thankful. Decide what for.");
+        states.add("Maybe you'll get lucky ;)");
+        states.add("Try practicing frugality.");
         states.add("Today you feel like a stable genius.");
-        states.add("Today you think you look confident but the world is undecided.");
-        states.add("Today you feel uncertain about the future.");
+        states.add("Are you confident?");
+        states.add("You are uncertain about the future.");
         states.add("Today your life does not make sense to you.");
-        states.add("Today you need to focus but that may be difficult.");
-        states.add("Today you are ambitious but beware!  ");
-        states.add("Today you are realistic regarding the weather.");
-        states.add("Today you will be unusually polite. Well done!");
-        states.add("Today you are ignorant but it will pass.");
+        states.add("Remember, bad times are just times that are bad.");
+        states.add("You got this.");
+        states.add("The weather will change soon.");
+        states.add("Recently you've been unusually polite. Well done!");
+        states.add("So it goes...");
+        states.add("You are what you pretend to be, so be careful what you pretend to be.");
+        states.add("There's many ways to skin that fish.");
+        states.add("Make it sexy!");
+        states.add("There is nothing as stable as change.");
+        states.add("Sit down. Be humble.");
+        states.add("You're inconsistent, even to yourself.");
+        states.add("If you're not busy being born you're busy dying.");
+        states.add("Be kind whenever possible. It is always possible.");
+        states.add("Did you lock the door?");
+        states.add("You seem worried. Maybe take up knitting?");
+        states.add("You look happy. Keep going!");
+        states.add("Re-evaluate your position. ");
+        states.add("It's not that bad.");
+        states.add("You don't know what you don't know.");
+
 
     }
 
